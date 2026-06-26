6b95a1910cc823e78cb7b40c3a8b70033513c595
cuckord <n77794474@gmail.com>
2026-06-26 05:43:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -537,6 +537,7 @@ export default function App() {
                   selectedYear={selectedYear} 
                   onToggleStatus={handleToggleFeesStatus}
                   onDelete={handleSoftDropStudent} 
+                  onSendWhatsApp={handleSendWhatsAppReminder}
                   onEdit={onEdit}
                 />
               );
